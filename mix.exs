c435c87ac161d226d396770ac07f8c9f9f00650d
Ilya Kuzmin <kuzmin@tochka.com>
2023-11-06 18:49:33 +0400

    a bit of config change

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FinancesBackend.MixProject do
     [
       app: :finances_backend,
       version: "0.1.0",
-      elixir: "1.15.6",
+      elixir: "1.15.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
