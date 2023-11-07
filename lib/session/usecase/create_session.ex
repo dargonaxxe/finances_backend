c594ecc207d5b5088911dbae42368b03c8c60cb9
Ilya Kuzmin <kuzmin@tochka.com>
2023-11-07 14:23:56 +0400

    implemented create_session usecase

---
+++ lib/session/usecase/create_session.ex
@@ -0,0 +1,26 @@
+defmodule FinancesBackend.Session.Usecase.CreateSession do
+  @moduledoc """
+  Usecase that creates session. Used in sign-in scenario
+  """
+  alias Finances.Repo
+  alias FinancesBackend.Session
+
+  @spec execute(charlist()) :: {:ok, Session} | {:error, any()}
+  def execute(user_id) do
+    params = %{
+      token: UUID.uuid1(),
+      user_id: user_id,
+      valid_until: valid_until()
+    }
+
+    %Session{}
+    |> Session.changeset(params)
+    |> Repo.insert()
+  end
+
+  @session_valid_days 1
+  defp valid_until() do
+    DateTime.utc_now()
+    |> DateTime.add(@session_valid_days, :day)
+  end
+end
