4ef10f39f3f92e8ff1df395c07b8d7ff08b49205
dargonaxxe <dargonaxxe@gmail.com>
2023-11-08 12:58:37 +0400

    added budget validations

--- lib/budget/budget.ex
+++ lib/budget/budget.ex
@@ -15,4 +15,17 @@ defmodule FinancesBackend.Budget do
     field :end_date, :date
     timestamps()
   end
+
+  import Ecto.Changeset
+
+  @name_min_length 3
+  def changeset(%__MODULE__{} = budget, %{} = params) do
+    budget
+    |> cast(params, [:name, :user_id, :allocated_money, :currency, :start_date, :end_date])
+    |> validate_required([:name, :user_id, :allocated_money, :currency, :start_date, :end_date])
+    |> validate_length(:name, min: @name_min_length)
+    |> assoc_constraint(:user)
+    |> check_constraint(:allocated_money, name: "allocated_money_should_be_positive")
+    |> check_constraint(:start_date, name: "budget_should_start_before_end")
+  end
 end
--- test/budget/budget_test.exs
+++ test/budget/budget_test.exs
@@ -20,4 +20,119 @@ defmodule FinancesBackend.BudgetTest do
     actual_fields = Budget.__schema__(:fields)
     assert MapSet.new(@expected_fields) == MapSet.new(actual_fields)
   end
+
+  @date Date.from_iso8601!("2023-01-01")
+  test "should require name field" do
+    budget = %Budget{}
+
+    params = %{
+      user_id: 0,
+      allocated_money: 1,
+      currency: 2,
+      start_date: @date,
+      end_date: @date
+    }
+
+    changeset = Budget.changeset(budget, params)
+    assert !changeset.valid?
+    [name: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should require user_id field" do
+    budget = %Budget{}
+
+    params = %{
+      name: "name",
+      allocated_money: 0,
+      currency: 1,
+      start_date: @date,
+      end_date: @date
+    }
+
+    changeset = Budget.changeset(budget, params)
+    assert !changeset.valid?
+
+    [user_id: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should require allocated_money field" do
+    budget = %Budget{}
+
+    params = %{
+      name: "name",
+      user_id: 0,
+      currency: 1,
+      start_date: @date,
+      end_date: @date
+    }
+
+    changeset = Budget.changeset(budget, params)
+    assert !changeset.valid?
+    [allocated_money: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should require currency field" do
+    budget = %Budget{}
+
+    params = %{
+      name: "name",
+      user_id: 0,
+      allocated_money: 1,
+      start_date: @date,
+      end_date: @date
+    }
+
+    changeset = Budget.changeset(budget, params)
+    assert !changeset.valid?
+    [currency: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should require start_date field" do
+    budget = %Budget{}
+
+    params = %{
+      name: "name",
+      user_id: 0,
+      allocated_money: 1,
+      currency: 1,
+      end_date: @date
+    }
+
+    changeset = Budget.changeset(budget, params)
+    assert !changeset.valid?
+    [start_date: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should require end_date field" do
+    budget = %Budget{}
+
+    params = %{
+      name: "name",
+      user_id: 0,
+      allocated_money: 1,
+      currency: 2,
+      start_date: @date
+    }
+
+    changeset = Budget.changeset(budget, params)
+    assert !changeset.valid?
+    [end_date: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should validate name length" do
+    budget = %Budget{}
+
+    params = %{
+      name: "na",
+      user_id: 0,
+      allocated_money: 1,
+      currency: 2,
+      start_date: @date,
+      end_date: @date
+    }
+
+    changeset = Budget.changeset(budget, params)
+    assert !changeset.valid?
+    [name: {_, [count: 3, validation: :length, kind: :min, type: :string]}] = changeset.errors
+  end
 end
