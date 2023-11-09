a705f21f21f97391d49b2a07cdef0cbc026aba51
Ilya Kuzmin <kuzmin@tochka.com>
2023-11-09 13:11:07 +0400

    implemented expense db-side validation & changeset

--- lib/expense/expense.ex
+++ lib/expense/expense.ex
@@ -3,6 +3,8 @@ defmodule FinancesBackend.Expense do
   Expense Ecto schema. Represent the expenses that happen within a range of dates for a 
   given budget. And for a given account. Can have an optional comment.
   """
+  alias Ecto.Changeset
+  alias FinancesBackend.Expense
   alias FinancesBackend.Account
   alias FinancesBackend.Budget
 
@@ -15,5 +17,17 @@ defmodule FinancesBackend.Expense do
     field :amount, :integer
     field :comment, :string
     timestamps()
+  end
+
+  import Ecto.Changeset
+
+  @spec changeset(Expense, Map) :: Changeset
+  def changeset(%__MODULE__{} = expense, %{} = params) do
+    expense
+    |> cast(params, [:budget_id, :account_id, :date, :amount, :comment])
+    |> validate_required([:budget_id, :account_id, :date, :amount])
+    |> assoc_constraint(:budget)
+    |> assoc_constraint(:account)
+    |> check_constraint(:amount, name: "amount_should_be_positive")
   end
 end
--- test/expense/expense_test.exs
+++ test/expense/expense_test.exs
@@ -18,4 +18,91 @@ defmodule FinancesBackend.ExpenseTest do
     actual_fields = Expense.__schema__(:fields)
     assert MapSet.new(@expected_fields) == MapSet.new(actual_fields)
   end
+
+  alias FinancesBackend.Expense
+  @date Date.from_iso8601!("2023-01-01")
+  test "should require budget_id" do
+    expense = %Expense{}
+
+    params = %{
+      account_id: 0,
+      date: @date,
+      amount: 1
+    }
+
+    changeset = Expense.changeset(expense, params)
+    assert !changeset.valid?
+    [budget_id: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should require account_id field" do
+    expense = %Expense{}
+
+    params = %{
+      budget_id: 0,
+      date: @date,
+      amount: 1
+    }
+
+    changeset = Expense.changeset(expense, params)
+    assert !changeset.valid?
+    [account_id: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should require date field" do
+    expense = %Expense{}
+
+    params = %{
+      account_id: 0,
+      budget_id: 1,
+      amount: 2
+    }
+
+    changeset = Expense.changeset(expense, params)
+    assert !changeset.valid?
+    [date: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should require amount field" do
+    expense = %Expense{}
+
+    params = %{
+      account_id: 0,
+      budget_id: 1,
+      date: @date
+    }
+
+    changeset = Expense.changeset(expense, params)
+    assert !changeset.valid?
+    [amount: {_, [validation: :required]}] = changeset.errors
+  end
+
+  test "should return no error without comment" do
+    expense = %Expense{}
+
+    params = %{
+      account_id: 0,
+      budget_id: 1,
+      date: @date,
+      amount: 2
+    }
+
+    changeset = Expense.changeset(expense, params)
+    assert changeset.valid?
+  end
+
+  test "should return no error with comment" do
+    expense = %Expense{}
+
+    params = %{
+      account_id: 0,
+      budget_id: 1,
+      date: @date,
+      amount: 2,
+      comment: "comment"
+    }
+
+    changeset = Expense.changeset(expense, params)
+    assert changeset.valid?
+  end
 end
