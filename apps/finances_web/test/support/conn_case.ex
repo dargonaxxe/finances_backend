e5e4a307dcf118cc38d8f0dc3ebc2ae0c7b2b413
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-05 14:24:18 +0400

    fixed failing tests

--- apps/finances_web/test/support/conn_case.ex
+++ apps/finances_web/test/support/conn_case.ex
@@ -31,8 +31,7 @@ defmodule FinancesWeb.ConnCase do
     end
   end
 
-  setup tags do
-    FinancesWeb.DataCase.setup_sandbox(tags)
+  setup _tags do
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
